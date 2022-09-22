eb9ecbba3306d9fb2493e6a01407fc2a50a148c1
MarissZZZ <marisszuss@gmail.com>
2022-09-22 13:47:07 +0300

    Json

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ async function iegutDarglietasNoAPi() { //darbība nenotiks vien aiz otras, var 
         console.log(datiJson)
 
 }
+
+async function iegutElektronikuNoAPi() { //darbība nenotiks vien aiz otras, var notikt citā laikā
+    let datiNoApi=await fetch ('https://fakestoreapi.com/products/category/electronics') //dati būs jāgaida
+    let datiJson=datiNoApi.json; //datu iegūšana
+        console.log(datiJson)
+
+}
